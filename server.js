9adf609c45039bd41ebd760c845cbda8c4e62fc7
Javier Arce <javierarce@users.noreply.github.com>
2019-05-07 10:49:11 +0200

    Fixes CORS

--- server.js
+++ server.js
@@ -100,14 +100,13 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
 
   if ('OPTIONS' === req.method) {
-    res.send(200);
-  }
-  else {
-    next();
+    res.send(200)
+  } else {
+    next()
   }
 })
 
