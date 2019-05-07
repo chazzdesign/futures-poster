87af6020acfe837eb4b9e6bfd9aabc5c89c6cb06
Javier Arce <javierarce@users.noreply.github.com>
2019-05-07 10:42:43 +0200

    Use cors

--- server.js
+++ server.js
@@ -99,6 +99,12 @@ const onWebhook = (req, res) => {
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next();
+})
+
 app.get('/', showHome)
 app.get('/api/fetch', fetchTweets)
 app.get('/api/future', getRandomTweet)
